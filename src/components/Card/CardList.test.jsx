25a496aee66862ff9b048866ec94727a530d0b60
Yasir326 <yasirk@nandos.co.uk>
2021-02-22 11:15:48 +0000

    remove unsued import

--- src/components/Card/CardList.test.jsx
+++ src/components/Card/CardList.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store'
 import CardList from './CardList.jsx';
