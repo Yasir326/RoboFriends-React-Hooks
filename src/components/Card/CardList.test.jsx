bdd6efbffe4f81f0e01f439b94a9f99d3e8ff09e
Yasir326 <yasirk@nandos.co.uk>
2021-02-22 11:11:15 +0000

    testing for cardlist

---
+++ src/components/Card/CardList.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
+import CardList from './CardList.jsx';
+import '../../setUpTest';
+
+const mockRobots = [
+  {
+    id: 1,
+    name: 'robocop',
+    email: 'robocop@gmail.com',
+    key: 1,
+  },
+];
+
+const mockStore = configureStore()
+const initialState = {isPending:true}
+let store
+
+it('expect to render Card component', () => {
+  store = mockStore(initialState)
+  // const cardlist = shallow(<Provider<CardList/></Provider>, mockRobots);
+  expect(
+      <Provider store={store}>
+        <CardList robots={mockRobots}/>
+      </Provider>
+  ).toMatchSnapshot();
+});
